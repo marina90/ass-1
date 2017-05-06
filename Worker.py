ca769e7a0fa44115a8e093dffc12a706ab68f6d6
marina90 <marina120890@gmail.com>
2017-05-06 15:00:31 +0300

    Add files via upload

--- Worker.py
+++ Worker.py
@@ -9,10 +9,11 @@ from pdfminer.pdfpage import PDFPage
 from wand.image import Image
 from wand.exceptions import WandException
 
-class Worker :
+class Worker:
     connection = boto3.resource(service_name='sqs')
     running = True
     error_occurred_while_trying_to_format = False
+
     '''
     listens to the sqs and extracts a message from the sqs ,then process's it by sending it to the run function 
     and then deletes it
@@ -20,11 +21,11 @@ class Worker :
     def pull_and_download(self):
         try:
             inQueue = self.connection.get_queue_by_name(QueueName="Manager-worker-queue")
-        except :
+        except:
             print "No sqs found waiting for one to open..."
             time.sleep (10)
             self.pull_and_download()
-        while self.running == True :
+        while self.running:
             for message in inQueue.receive_messages(VisibilityTimeout=30, MessageAttributeNames=['All']):
                 attributes = message.message_attributes
                 if message.body == "terminate":
@@ -51,33 +52,33 @@ class Worker :
     '''
     gets a complete msg (what to do and on which url ) and checks which action needs to be preformed 
     '''
-    def  run_task(self,msg):
-        parsed_message =msg.split("\t")
+    def run_task(self, msg):
+        parsed_message = msg.split("\t")
         processed_data = "wrong message format ,nothing changed "
         if parsed_message[0] == "ToImage":
             print "Trying to convert to Image"
-            processed_data=self.convert_to_image(parsed_message[1])
+            processed_data = self.convert_to_image(parsed_message[1])
         elif parsed_message[0] == "ToHTML":
             print "Trying to convert to HTML"
-            processed_data =self.convert_to_html(parsed_message[1])
+            processed_data = self.convert_to_html(parsed_message[1])
         elif parsed_message[0] == "ToText":
             print "Trying to convert to Text"
-            processed_data =self.convert_to_text(parsed_message[1])
-        else :
+            processed_data = self.convert_to_text(parsed_message[1])
+        else:
             print "error : Wrong message format "
         return processed_data
 
     '''
     gets a msg and attributes and sends the message with the attributes to the sqs 
     '''
-    def  send_to_sqs(self, msg, attributes):
-        outQueue=self.connection.get_queue_by_name(QueueName="Worker-manager-queue")
-        outQueue.send_message(MessageBody=msg, MessageAttributes = attributes)
+    def send_to_sqs(self, msg, attributes):
+        outQueue = self.connection.get_queue_by_name(QueueName="Worker-manager-queue")
+        outQueue.send_message(MessageBody=msg, MessageAttributes=attributes)
 
     '''
     gets a file and uploads it to s3 bucket
     '''
-    def upload(self, to_upload) :
+    def upload(self, to_upload):
         s3 = boto3.client(service_name='s3')
         s3.upload_file(to_upload, "dsp1-bucket-gn", to_upload)
         return "https://s3.amazonaws.com/{}/{}".format("dsp1-bucket-gn", to_upload)
@@ -85,16 +86,16 @@ class Worker :
     '''
     gets a url and converts the message to an image file 
     '''
-    def convert_to_image(self,msg):
+    def convert_to_image(self, msg):
         filename = msg.rsplit('/', 1)[1]
         filename = filename[:-4]
-        try :
+        try:
             with Image(filename=msg+"[0]", resolution=200) as img:
                 img.save(filename=filename + ".png")
         except TypeError as e:
             return self.making_an_error_message(e.message)
         except WandException as e:
-            return self.making_an_error_message(e.message)
+            return self.making_an_error_message('Wand Exception while trying to convert')
         except:
             return self.making_an_error_message("unexpected error")
         try:
@@ -129,7 +130,7 @@ class Worker :
             interpreter.process_page(page)
             data = retstr.getvalue()
             break
-        upload_file=open(filename, 'w')
+        upload_file = open(filename, 'w')
         upload_file.write(data)
         return filename
 
@@ -140,11 +141,11 @@ class Worker :
         testfile = urllib.URLopener()
         filename = msg.rsplit('/', 1)[1]
         filename = filename[:-4] + ".html"
-        try :
+        try:
             testfile.retrieve(msg, filename)
         except IOError as e:
             return self.making_an_error_message(e)
-        except :
+        except:
             return self.making_an_error_message("unexpected error")
         fp = file(filename, 'rb')
         rsrcmgr = PDFResourceManager()
@@ -164,11 +165,9 @@ class Worker :
     '''
     gets an exception and returns a message 
     '''
-    def making_an_error_message(self ,e):
+    def making_an_error_message(self, e):
         self.error_occurred_while_trying_to_format = True
         return e
-
-
 
 work = Worker()
 work.pull_and_download()
