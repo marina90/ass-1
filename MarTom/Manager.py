d7fa9441831ae18f7319723dd9706493ab853cbf
marina90 <marina120890@gmail.com>
2017-05-07 23:21:19 +0300

    test

---
+++ MarTom/Manager.py
@@ -0,0 +1,173 @@
+import boto3
+import botocore
+import time
+from botocore.exceptions import ClientError
+
+
+class Manager:
+    def __init__(self):
+        self.should_terminate = False
+        self.bucket_name = 'dsp1-bucket'
+        self.sqs_names = ['Manager-Worker-queue', 'Worker-Manager-queue', 'Local-Manager-queue', 'Manager-Local-queue']
+        self.sqs = boto3.resource(service_name='sqs')
+        self.ec2 = boto3.resource(service_name='ec2')
+        self.s3 = boto3.client(service_name='s3')
+        self.s3_resource = boto3.resource(service_name='s3')
+        self.start_sqs()
+
+    def start_sqs(self):
+        for i in range(len(self.sqs_names)):
+            self.sqs.create_queue(QueueName=self.sqs_names[i])
+
+    def start_workers(self, n):
+        max_num_of_instances = 5
+        user_data = '''#!/bin/bash
+                                    apt-get update
+                                    --nogpgcheck --skip-broken -y dos2unix glances screen gcc make python-devel python-setuptools python-pip git rubygems rpmbuild ruby-devel
+                                    yum -y install --nogpgcheck ImageMagick
+                                    yum -y install --nogpgcheck python2.7
+                                    apt-get install python2.7
+                                    git clone  https://github.com/marina90/ass-1
+                                    sudo apt-get install imagemagick
+                                    pip install boto3
+                                    pip install botocore
+                                    pip install pdfminer
+                                    pip install wand
+                                    pip install -r ass-1/requirements.txt
+                                    python ass-1/MarTom/Worker.py
+                                    '''
+        current_amount_of_instances = 0
+        instances = self.ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'initializing', 'pending']}])
+        for instance in instances:
+            current_amount_of_instances += 1
+        n = min(n, max_num_of_instances)
+        if current_amount_of_instances < n:
+            try:
+                self.ec2.create_instances(ImageId='ami-bb6801ad', MinCount=1, MaxCount=n, InstanceType='t1.micro',
+                                          KeyName='KeyPair', SecurityGroups=['default'], UserData=user_data)
+            except Exception as e:
+                print e
+        time.sleep(5)
+        instances = self.ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'initializing', 'pending']}])
+        for instance in instances:
+            if instance.tags is None:
+                instance.create_tags(Tags=[{'Key': 'Role', 'Value': 'Worker'}])
+
+    def terminate_workers(self):
+        instances = self.ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'initializing', 'pending']}])
+        for instance in instances:
+            if instance.tags[0]['Value'] == 'Worker':
+                self.ec2.instances.filter(InstanceIds=[instance.id]).terminate()
+
+    def send(self, msg, local_name, num_of_lines, sqs_name):
+        queue = self.sqs.get_queue_by_name(QueueName=sqs_name)
+        attributes = {
+            "LocalName": {
+                "DataType": "String",
+                "StringValue": local_name
+            },
+            "NumOfLines": {
+                "DataType": "Number",
+                "StringValue": str(num_of_lines)
+            }
+        }
+        queue.send_message(MessageAttributes=attributes, MessageBody=msg)
+
+    def wait_for_message(self, sqs_name):
+        queue = self.sqs.get_queue_by_name(QueueName=sqs_name)
+        for i in range(10):
+            for message in queue.receive_messages(VisibilityTimeout=30, MessageAttributeNames=['All']):
+                self.do(message)
+                message.delete()
+
+    def count_instances(self):
+        instances = ec2.instances.filter(
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'initializing', 'pending']}])
+        instance_counter = 0
+        for instance in instances:
+            instance_counter += 1
+        return instance_counter
+
+    def send_terminate(self):
+        self.terminate_workers()
+        self.send_message_with_attributes('manager terminated', local_name, 0, self.sqs_names[3])
+        self.should_terminate = True
+
+    def do(self, message):
+        local_name = message.message_attributes.get('LocalName').get('StringValue')
+        if message.body == 'terminate':
+            self.counter = self.count_instances()
+            for i in range(self.counter - 1):
+                self.send_message_with_attributes('terminate', local_name, 0, self.sqs_names[0])
+        elif message.body == 'worker terminated':
+            self.counter -= 1
+            if self.counter == 0:
+                self.send_terminate()
+        else:
+            parsed_message = message.body.split('\t')
+            if parsed_message[0] == 'job':
+                self.start_workers(int(float(parsed_message[2])))
+                self.split_and_send(parsed_message[1], message)
+            else:  # finished task
+                self.makeHtmlFile(message)
+
+    def split_and_send(self, input_file, message):
+        self.s3_resource.meta.client.download_file(self.bucket_name, input_file, input_file)
+        local_name = message.message_attributes.get('LocalName').get('StringValue')
+        with open(input_file) as inputFile:
+            numOfLines = sum(1 for _ in inputFile)
+        with open(input_file) as inputFile:
+            for line in inputFile:
+                print line[:-1]
+                self.send(line, local_name, numOfLines, self.sqs_names[0])
+
+    def upload(self, to_upload):
+        self.s3.upload_file(to_upload, self.bucket_name, to_upload)
+
+    def makeHtmlFile(self, message):
+        fileName = message.message_attributes.get('LocalName').get('StringValue') + '.html'
+        exists = False
+        try:
+            self.s3_resource.Object(self.bucket_name, fileName).load()
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                exists = False
+            else:
+                print e
+        else:
+            exists = True
+        line = message.body
+        numOfLines = int(message.message_attributes.get('NumOfLines').get('StringValue'))
+        if not exists:
+            with open(fileName, 'w') as outputFile:
+                outputFile.write("<html><body>" + line + "<br>\n")
+                n = 1
+        else:
+            with open(fileName, 'a') as outputFile:
+                outputFile.write(line + "<br>\n")
+            with open(fileName, 'r') as outputfile:
+                n = sum(1 for _ in outputfile)
+        if (n == numOfLines):
+            with open(fileName, 'a') as outputFile:
+                outputFile.write("</body></html>")
+            self.upload(fileName)
+            localName = message.message_attributes.get('LocalName').get('StringValue')
+            numOfLines = int(message.message_attributes.get('NumOfLines').get('StringValue'))
+            self.send("Done html", localName, numOfLines, self.sqs_names[3])
+        else:
+            self.upload(fileName)
+
+
+def main():
+    manager = Manager()
+    while not manager.should_terminate:
+        print 'switch'
+        manager.wait_for_message(manager.sqs_names[2])
+        manager.wait_for_message(manager.sqs_names[1])
+
+
+if __name__ == "__main__":
+    main()
---
+++ MarTom/Worker.py
@@ -0,0 +1,163 @@
+import urllib
+from cStringIO import StringIO
+import boto3
+import time
+from pdfminer.converter import HTMLConverter, TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from wand.image import Image
+from wand.exceptions import WandException
+
+
+class Worker :
+
+    def __init__(self):
+        self.connection = boto3.resource(service_name='sqs')
+        self.active = True
+        self.format_error_flag = False
+        self.sqs_names =['Manager-Worker-queue', 'Worker-Manager-queue']
+        self.s3_bucket_name = 'dsp1-bucket'
+
+    def convert(self, msg):
+        parsed_message = msg.split("\t")
+        processed_data = "Incorrect message format "
+        if parsed_message[0] == "ToImage":
+            processed_data = self.convert_to_image(parsed_message[1])
+        elif parsed_message[0] == "ToHTML":
+            processed_data = self.convert_to_html(parsed_message[1])
+        elif parsed_message[0] == "ToText":
+            processed_data = self.convert_to_text(parsed_message[1])
+        else:
+            print "error : Incorrect message format "
+        return processed_data
+
+    def send_to_manager(self, msg, attributes):
+        outQueue=self.connection.get_queue_by_name(QueueName=self.sqs_names[1])
+        outQueue.send_message(MessageBody = msg, MessageAttributes = attributes)
+
+    def upload(self, to_upload):
+        s3 = boto3.client(service_name='s3')
+        s3.upload_file(to_upload, self.s3_bucket_name, to_upload)
+        return "https://s3.amazonaws.com/" + self.s3_bucket_name + '/' + to_upload
+
+    def convert_to_image(self,msg):
+        filename = msg.rsplit('/', 1)[1]
+        filename = filename[:-4]
+        try :
+            with Image(filename=msg+"[0]", resolution=200) as img:
+                img.save(filename=filename + ".png")
+        except TypeError as e:
+            self.format_error_flag = True
+            return e.message
+        except WandException as e:
+            self.format_error_flag = True
+            return "WandException error"
+        except :
+            self.format_error_flag = True
+            return "Unexpected error"
+        try:
+            with Image(filename=filename + ".png") as img:
+                img.resize(200, 150)
+                img.save(filename=filename + ".png")
+        except :
+            self.format_error_flag = True
+            return "Unexpected error"
+        return filename + ".png"
+
+    def convert_to_text(self,msg):
+        testfile = urllib.URLopener()
+        filename = msg.rsplit('/', 1)[1]
+        filename = filename[:-4] + ".txt"
+        try :
+            testfile.retrieve(msg, filename)
+        except IOError as e :
+            self.format_error_flag = True
+            return e
+        except :
+            self.format_error_flag = True
+            return "Unexpected error"
+        fp = file(filename, 'rb')
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(fp):
+            interpreter.process_page(page)
+            data = retstr.getvalue()
+            break
+        upload_file=open(filename,'w')
+        upload_file.write(data)
+        return filename
+
+    def convert_to_html(self,msg):
+        testfile = urllib.URLopener()
+        filename = msg.rsplit('/', 1)[1]
+        filename = filename[:-4] + ".html"
+        try :
+            testfile.retrieve(msg, filename)
+        except IOError as e:
+            self.format_error_flag = True
+            return e
+        except :
+            self.format_error_flag = True
+            return "Unexpected error"
+        fp = file(filename, 'rb')
+        rsrcmgr = PDFResourceManager()
+        retstr = StringIO()
+        codec = 'utf-8'
+        laparams = LAParams()
+        device = HTMLConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.get_pages(fp):
+            interpreter.process_page(page)
+            data = retstr.getvalue()
+            break
+        upload_file = open(filename, 'w')
+        upload_file.write(data)
+        return filename
+
+
+def main():
+    readyToListen=False
+    while not readyToListen:
+        try:
+            inQueue = worker.connection.get_queue_by_name(QueueName=worker.sqs_names[0])
+            readyToListen=True
+        except Exception as e:
+            print e
+            print "The queue: " + worker.sqs_names[0] + " is not available yet. Please wait."
+            time.sleep(10)
+    while worker.active:
+        try:
+            for message in inQueue.receive_messages(VisibilityTimeout=30, MessageAttributeNames=['All']):
+                attributes = message.message_attributes
+                if (message.body == "terminate"):
+                    worker.active = False
+                    terminate_message = 'worker terminated'
+                    terminate_message = terminate_message.replace("\n", " ")
+                    message.delete()
+                    worker.send_to_manager(terminate_message, attributes)
+                else:  # message that need to be processed
+                    file_to_upload = worker.convert(message.body)
+                    if worker.format_error_flag or file_to_upload == "Incorrect message format ":
+                        msg = message.body + "\t" + str(file_to_upload)
+                        msg = msg.replace("\n", " ")
+                        worker.send_to_manager(msg, attributes)
+                        worker.format_error_flag = False
+                    else:
+                        url_of_s3 = worker.upload(file_to_upload)  # uploading the new data to the s3
+                        parsed_message = message.body.split("\t")  # 0 - action ,1 - url
+                        msg = parsed_message[1] + "\t" + url_of_s3 + "\t" + parsed_message[0]
+                        msg = msg.replace("\n", " ")
+                        worker.send_to_manager(msg, attributes)
+                    message.delete()
+        except Exception as e:
+            print e
+
+
+if __name__ == "__main__":
+    worker = Worker()
+    main()
